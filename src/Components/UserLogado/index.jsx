c0782d77e4f036abd97765f22d9809f0f77fea42
Kevyn Marques <kevynmarquesdev@hotmail.com>
2022-11-04 14:48:07 -0300

    atualização 04/11

--- src/Components/UserLogado/index.jsx
+++ src/Components/UserLogado/index.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import { useState } from "react";
+import { BoxHome } from "../Login/style";
+import { InconName, TextName } from "./style";
 
 
 export const UserLogado = ()=>{
+
+   const Name = sessionStorage.getItem('@AuthFirebase:user')
+   const key  = JSON.parse(Name); 
+
  
-  const teste = localStorage.getItem('email'); 
-  const teste2 = JSON.parse(teste); 
-  console.log(teste2); 
+   function firstLetters(str) {
+    const names = str.split(' ')
+    let initials = ''
 
+    for (let index = 0; index < names.length; index++) {
+      initials += `${names[index][0]}`
+    }
+    return  initials; 
+  }
+  
 
- const [nameUser, setNameUser] = useState ('Qualquer coisa'); 
-   return(
-     <h1 > {nameUser} </h1>
+  const [nameUser, setNameUser] = useState (firstLetters(key.displayName)); 
+
+ 
+    
+   return ( 
+     <BoxHome>
+      <InconName>
+        <TextName>
+           {nameUser}
+        </TextName>
+      </InconName>
+    </BoxHome>
    );
 }
---
+++ src/Components/UserLogado/style.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const InconName = styled.div`
+background-color:  white;
+width: 70px;
+height: 70px;
+border-radius: 50px;
+position: absolute;
+margin-top: -540px;
+margin-right: -1250px;
+`;
+
+export const TextName = styled.p`
+ font-family: Arial, Helvetica, sans-serif;
+ padding: 25px;
+`; 
